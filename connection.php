babea7a18b7a81696c3e5a7d523e94f4ae1bb387
Raj Singh Rajput <114401188+rajdevl01@users.noreply.github.com>
2023-12-30 13:32:42 +0530

    Update connection.php

--- connection.php
+++ connection.php
@@ -1,1 +1,15 @@
 
+
+<?php
+try{
+    $dbname = "mysql:host=localhost;dbname=students";
+    $username = "root";
+    $password = "";
+    $conn = new PDO($dbname,$username,$password);
+
+
+} catch(Exception $e){
+    echo "Message ". $e->getMessage();
+}
+
+ ?>
